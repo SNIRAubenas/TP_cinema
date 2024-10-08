2e8c14376da2d4dda4f77ba976f3ce2dd9416078
arthurMoirenc <arthur.moirenc@lyceeastier.com>
2024-10-08 10:42:59 +0200

    ajout des classes de Gestion

---
+++ Application_Cinema/Gestion/FilmDirector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Cinema.Gestion
+{
+    internal class FilmDirector
+    {
+        public int Id { get; set; } 
+        public int IdFilm { get; set; }
+        public int IdDirector { get; set; }
+    }
+}
