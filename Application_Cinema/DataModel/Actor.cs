2e540f1f50c42d343302539962455e8674457ad1
LucasSGx <lucas.sangeroteo@lyceeastier.com>
2024-10-09 16:32:28 +0200

    debut de la fusion de nos travaux

--- Application_Cinema/DataModel/Actor.cs
+++ Application_Cinema/DataModel/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
 using System.Collections.Generic;
 
 namespace Application_Cinema.DataModel;
@@ -9,5 +10,5 @@ public partial class Actor
 
     public string Name { get; set; } = null!;
 
-    public virtual ICollection<Filmactor> Filmactors { get; set; } = new List<Filmactor>();
+    public virtual ObservableCollectionListSource<Filmactor> Filmactors{ get; set; } = new ObservableCollectionListSource<Filmactor>(); 
 }
