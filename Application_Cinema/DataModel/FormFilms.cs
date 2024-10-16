9a6ff3e351b3092c467cf679e64dbb30153434bb
LucasSGx <lucas.sangeroteo@lyceeastier.com>
2024-10-16 15:22:28 +0200

    Film terminer

--- Application_Cinema/DataModel/FormFilms.cs
+++ Application_Cinema/DataModel/FormFilms.cs
@@ -43,7 +43,47 @@ namespace Application_Cinema.DataModel
 
         private void ajouter_Click(object sender, EventArgs e)
         {
+            string titre = Microsoft.VisualBasic.Interaction.InputBox("Titre du film :", "Ajouter un Film");
+            string anneeStr = Microsoft.VisualBasic.Interaction.InputBox("Année de sortie :", "Ajouter un Film");
+            string longueurStr = Microsoft.VisualBasic.Interaction.InputBox("Durée du film (format hh:mm) :", "Ajouter un Film");
+            string resume = Microsoft.VisualBasic.Interaction.InputBox("Résumé du film :", "Ajouter un Film");
 
+            if (string.IsNullOrWhiteSpace(titre) ||!int.TryParse(anneeStr, out int annee) || !TimeSpan.TryParseExact(longueurStr, @"hh\:mm", null, out TimeSpan longueur) ||  string.IsNullOrWhiteSpace(resume))
+            {
+                MessageBox.Show("Veuillez remplir correctement tous les champs.\nAssurez-vous que l'année est un nombre valide et la durée au format hh:mm.");
+                return;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Fichiers d'image|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Veuillez sélectionner une image pour le poster.");
+                return;
+            }
+
+            byte[] imageBytes;
+            using (var stream = new System.IO.MemoryStream())
+            {
+                Image image = Image.FromFile(openFileDialog.FileName);
+                image.Save(stream, image.RawFormat); 
+                imageBytes = stream.ToArray(); 
+            }
+
+            var nouveauFilm = new Film()
+            {
+                Title = titre,
+                Year = annee,  
+                Length = longueur,  
+                Summary = resume,
+                Poster = imageBytes 
+            };
+
+            this.dbContext.Films.Add(nouveauFilm);
+
+            this.dbContext.SaveChanges();
+
+            dataGridView1.DataSource = dbContext.Films.Local.ToBindingList();
         }
 
         private void modifier_Click(object sender, EventArgs e)
